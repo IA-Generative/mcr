9b38b7ca2ec9a9de3a6f43b805269b8847b62071
Thomas Metzger <thomas.metzger@theodo.com>
2026-01-23 16:17:14 +0100

    :recycle: (929): refactor time formatter

--- mcr-frontend/src/utils/timeFormatting.ts
+++ mcr-frontend/src/utils/timeFormatting.ts
@@ -5,16 +5,20 @@ export function formatDurationMinutes(durationMinutes: number | undefined): stri
     return t('meeting.transcription.transcription-in-queue.computing-estimation');
   }
 
-  if (durationMinutes <= 60) {
-    return t('meeting.transcription.transcription-generation-in-progress.less-than-1-hour');
-  }
-
   const hours = Math.floor(durationMinutes / 60);
   const minutes = durationMinutes % 60;
 
   if (hours >= 24) {
-    return `${hours} heures`;
+    return t('meeting.transcription.transcription-in-queue.duration.more-than-24h');
   }
 
-  return minutes < 10 ? `${hours}h0${minutes}` : `${hours}h${minutes}`;
+  const parts: string[] = [];
+  if (hours > 0) parts.push(`${hours}h`);
+  if (minutes > 0) parts.push(`${minutes}min`);
+
+  const formattedTime = parts.join(' ');
+
+  return t('meeting.transcription.transcription-in-queue.duration.less-than', {
+    formattedTime,
+  });
 }
