56d0b9e737f37f1deb0b289a05ed186fe9a89dde
EliNoden <eliott.morcillo@theodo.com>
2026-02-11 14:04:31 +0100

    ✨ (986): new MeetingListPageV2 component

---
+++ mcr-frontend/src/views/meeting/MeetingListPageV2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="fr-container py-5 flex w-full flex-col gap-10">
+    <PageFrontMatterV2
+      :title="$t('meetings_v2.hero.title')"
+      :subtitle="$t('meetings_v2.hero.subtitle')"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PageFrontMatterV2 from '@/components/core/PageFrontMatterV2.vue';
+</script>
