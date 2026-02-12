2db9439f4a063da955a9468efd50f56c44fc33bb
theodore2leusse <deleussetheodore@gmail.com>
2026-02-12 17:15:59 +0100

    :recycle: (978) use function to get the model for the make eval cli

--- mcr-core/mcr_meeting/app/services/speech_to_text/utils/models.py
+++ mcr-core/mcr_meeting/app/services/speech_to_text/utils/models.py
@@ -2,7 +2,16 @@
 
 from faster_whisper import WhisperModel  # type: ignore[import]
 from loguru import logger
-from pyannote.audio import Pipeline  # type: ignore[import]
+from pyannote.audio import Pipeline
+
+from mcr_meeting.app.configs.base import Settings, Speech2TextSettings
+from mcr_meeting.app.utils.compute_devices import ComputeDevice
+from mcr_meeting.app.utils.load_speech_to_text_model import (
+    load_diarization_pipeline,  # type: ignore[import]
+)
+
+settings = Settings()
+speech2text_settings = Speech2TextSettings()
 
 
 def get_transcription_model() -> WhisperModel:
@@ -11,6 +20,10 @@ def get_transcription_model() -> WhisperModel:
     Returns:
         WhisperModel: the whisper model used for transcription.
     """
+    if settings.ENV_MODE == "DEV":
+        model_name = speech2text_settings.STT_MODEL
+        model = WhisperModel(model_name)
+        return model
     try:
         logger.debug("loading model from celery context")
         from mcr_meeting.transcription_worker import context
@@ -33,6 +46,9 @@ def get_diarization_pipeline() -> Pipeline:
     Returns:
         Pipeline: the pyannote pipeline used for diarization.
     """
+    if settings.ENV_MODE == "DEV":
+        diarization_pipeline = load_diarization_pipeline(ComputeDevice.CPU)
+        return diarization_pipeline
     try:
         logger.debug("loading diarization pipeline from celery context")
         from mcr_meeting.transcription_worker import context
--- mcr-core/mcr_meeting/evaluation/cli/cli.py
+++ mcr-core/mcr_meeting/evaluation/cli/cli.py
@@ -1,8 +1,6 @@
-# mypy: ignore-errors
 import os
 from pathlib import Path
 
-from faster_whisper import WhisperModel  # type: ignore[import]
 from loguru import logger
 
 from mcr_meeting.evaluation.asr_evaluation_pipeline import ASREvaluationPipeline
@@ -12,7 +10,6 @@ from mcr_meeting.evaluation.cli.utils import load_audio_inputs
 def main() -> None:
     data_dir = Path(os.environ.get("EVALUATION_DATA_DIR", "data"))
     out_dir = Path(os.environ.get("EVALUATION_OUTPUT_DIR", data_dir / "outputs"))
-    model_name = os.environ.get("MODEL", "tiny")
 
     audio_dir = data_dir / "inputs" / "raw_audios"
     ref_dir = data_dir / "inputs" / "reference_transcripts"
@@ -27,8 +24,7 @@ def main() -> None:
         logger.error("No audio files → nothing to do.")
         exit(1)
 
-    model = WhisperModel(model_name)
-    pipeline = ASREvaluationPipeline(inputs=evaluation_inputs, model=model)
+    pipeline = ASREvaluationPipeline(inputs=evaluation_inputs)
     summary = pipeline.run_evaluation(output_dir=out_dir)
 
     logger.info("\n=== Evaluation Metrics ===")
