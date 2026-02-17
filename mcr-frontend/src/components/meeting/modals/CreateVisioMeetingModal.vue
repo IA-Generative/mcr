bef1f1b10a2a93f602b27584b838e718f13e579b
Léo Germain <leogermain@MacBook-Air-de-Leo.local>
2026-02-17 14:58:16 +0100

    :sparkles: (1091): create new visio modal

---
+++ mcr-frontend/src/components/meeting/modals/CreateVisioMeetingModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseModal
+    :modal-id="CREATE_MEETING_MODAL_ID"
+    :title="$t('meetingV2.form.title')"
+    size="lg"
+    class="max-sm:p-4"
+    :actions="[
+      {
+        label: $t('common.cancel'),
+        secondary: true,
+        onClick: () => close(),
+      },
+    ]"
+  >
+    <img src="/path_img" />
+  </BaseModal>
+</template>
+
+<script setup lang="ts">
+import BaseModal from '@/components/core/BaseModal.vue';
+import { useVfm } from 'vue-final-modal';
+
+const CREATE_MEETING_MODAL_ID = 'meeting-visio-modal-V2';
+const close = () => useVfm().close(CREATE_MEETING_MODAL_ID);
+</script>
