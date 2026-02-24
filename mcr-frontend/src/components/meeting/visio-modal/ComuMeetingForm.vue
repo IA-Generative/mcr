1d74888103a95eeb070d5dd23cc689a154c0668e
Léo Germain <leo.germain@theodo.com>
2026-02-24 14:47:45 +0100

    :sparkles: (1099): add COMU connection input fields

---
+++ mcr-frontend/src/components/meeting/visio-modal/ComuMeetingForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <DsfrInputGroup
+    class="m-0"
+    :label="$t('meeting-v2.visio-form.comu.url')"
+    :hint="$t('meeting-v2.visio-form.comu.url_hint')"
+    label-visible
+  />
+
+  <div class="flex flex-row gap-x-6">
+    <DsfrInputGroup
+      class="w-full flex-1"
+      :label="$t('meeting-v2.visio-form.comu.access_code')"
+      label-visible
+    />
+
+    <DsfrInputGroup
+      class="w-full flex-1"
+      :label="$t('meeting-v2.visio-form.comu.meeting_id')"
+      label-visible
+    />
+  </div>
+
+</template>
+
+<style scoped>
+:deep(.fr-input-group) {
+  flex-grow: 1;
+}
+</style>
