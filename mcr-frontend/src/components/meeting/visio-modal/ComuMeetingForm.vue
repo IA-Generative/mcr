636ae7d3c7091d3b5d5431574344e91cae3590f6
Léo Germain <leo.germain@theodo.com>
2026-02-24 14:48:44 +0100

    :sparkles: (1099): add custom separator

--- mcr-frontend/src/components/meeting/visio-modal/ComuMeetingForm.vue
+++ mcr-frontend/src/components/meeting/visio-modal/ComuMeetingForm.vue
@@ -6,6 +6,10 @@
     label-visible
   />
 
+  <div class="separator pt-2 pb-8">
+    <span>{{ $t('common.or') }}</span>
+  </div>
+
   <div class="flex flex-row gap-x-6">
     <DsfrInputGroup
       class="w-full flex-1"
@@ -19,10 +23,32 @@
       label-visible
     />
   </div>
-
 </template>
 
 <style scoped>
+.separator {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000;
+  font-weight: 500;
+}
+
+.separator::before,
+.separator::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid #ddd;
+}
+
+.separator::before {
+  margin-right: 10px;
+}
+
+.separator::after {
+  margin-left: 10px;
+}
+
 :deep(.fr-input-group) {
   flex-grow: 1;
 }
