3217b962140941f74e2eb9e9047d76de74965786
Léo Germain <leo.germain@theodo.com>
2026-02-25 17:03:58 +0100

    :sparkles: (1111): add visio, webconf & webinaire form components

---
+++ mcr-frontend/src/components/meeting/visio-modal/WebconfMeetingForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <DsfrInputGroup
+    v-model="webconfUrl"
+    class="m-0"
+    :label="$t('meeting-v2.visio-form.webconf.url')"
+    :hint="$t('meeting-v2.visio-form.webconf.url_hint')"
+    :error-message="webconfUrlError"
+    label-visible
+  />
+</template>
+
+<script setup lang="ts">
+import { t } from '@/plugins/i18n';
+import { webconfUrlValidator } from '../meeting.schema';
+
+const webconfUrl = ref<string>('');
+
+const webconfUrlError = computed(() => {
+  if (!webconfUrl.value) return '';
+  if (!webconfUrlValidator.test(webconfUrl.value)) {
+    return t('meeting-v2.visio-form.webconf.url_error');
+  }
+  return '';
+});
+</script>
---
+++ mcr-frontend/src/components/meeting/visio-modal/WebinaireMeetingForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <DsfrInputGroup
+    v-model="webinaireUrl"
+    class="m-0"
+    :label="$t('meeting-v2.visio-form.webinaire.url')"
+    :hint="$t('meeting-v2.visio-form.webinaire.url_hint')"
+    :error-message="webinaireUrlError"
+    label-visible
+  />
+
+  <VisioConnectionSeparator />
+
+  <DsfrInputGroup
+    class="m-0"
+    :label="$t('meeting-v2.visio-form.webinaire.connection_code')"
+    label-visible
+  />
+</template>
+
+<script setup lang="ts">
+import { t } from '@/plugins/i18n';
+import { webinaireModeratorUrlValidator } from '../meeting.schema';
+
+const webinaireUrl = ref<string>('');
+
+const webinaireUrlError = computed(() => {
+  if (!webinaireUrl.value) return '';
+  if (!webinaireModeratorUrlValidator.test(webinaireUrl.value)) {
+    return t('meeting-v2.visio-form.webinaire.url_error');
+  }
+  return '';
+});
+</script>
