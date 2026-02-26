b2c5a0454a79731647ce27cc0b1fbb21be9f09ef
Léo Germain <leo.germain@theodo.com>
2026-02-26 17:25:17 +0100

    :sparkles: (1118): submit for webconf

--- mcr-frontend/src/components/meeting/visio-modal/WebconfMeetingForm.vue
+++ mcr-frontend/src/components/meeting/visio-modal/WebconfMeetingForm.vue
@@ -7,12 +7,27 @@
     :error-message="webconfUrlError"
     label-visible
   />
+
+  <hr />
+
+  <div class="flex justify-end">
+    <DsfrButton
+      :label="$t('meeting-v2.visio-form.submit')"
+      icon="fr-icon-play-circle-fill"
+      :disabled="!isSubmitEnabled"
+      @click="handleSubmit()"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { t } from '@/plugins/i18n';
 import { webconfUrlValidator } from '../meeting.schema';
+import type { AddOnlineMeetingDto } from '@/services/meetings/meetings.types';
 
+const props = defineProps<{
+  title: string;
+}>();
 const webconfUrl = ref<string>('');
 
 const webconfUrlError = computed(() => {
@@ -22,4 +37,25 @@ const webconfUrlError = computed(() => {
   }
   return '';
 });
+
+const isSubmitEnabled = computed(() => {
+  return props.title !== '' && webconfUrlValidator.test(webconfUrl.value);
+});
+
+const emit = defineEmits<{
+  submit: [form: AddOnlineMeetingDto];
+  cancel: [];
+}>();
+
+function handleSubmit() {
+  const dto: AddOnlineMeetingDto = {
+    name: props.title,
+    url: webconfUrl.value,
+    name_platform: 'WEBCONF',
+    meeting_password: null,
+    meeting_platform_id: null,
+    creation_date: new Date().toISOString(),
+  };
+  emit('submit', dto);
+}
 </script>
