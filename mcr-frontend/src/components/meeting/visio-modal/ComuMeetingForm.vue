7c60ccc1a3b31d3b2988063709871caf514520f6
Léo Germain <leo.germain@theodo.com>
2026-03-06 11:57:36 +0100

    :lipstick: (1129): disable url and id/password simultaneous filling

--- mcr-frontend/src/components/meeting/visio-modal/ComuMeetingForm.vue
+++ mcr-frontend/src/components/meeting/visio-modal/ComuMeetingForm.vue
@@ -6,41 +6,63 @@
     :hint="$t('meeting-v2.visio-form.comu.url_hint')"
     :error-message="comuUrlError"
     label-visible
+    :disabled="!isUrlEnabled"
   />
 
   <VisioConnectionSeparator />
 
   <div class="flex flex-row gap-x-6 pb-4">
     <DsfrInputGroup
-      v-model="comuPassword"
-      class="w-full flex-1"
-      :label="$t('meeting-v2.visio-form.comu.access_code')"
-      :error-message="comuPasswordError"
-      label-visible
-    />
-
-    <DsfrInputGroup
       v-model="comuId"
       class="w-full flex-1"
       :label="$t('meeting-v2.visio-form.comu.meeting_id')"
       :error-message="comuIdError"
       label-visible
+      :disabled="!isIdPasswordEnabled"
+    />
+
+    <DsfrInputGroup
+      v-model="comuPassword"
+      class="w-full flex-1"
+      :label="$t('meeting-v2.visio-form.comu.access_code')"
+      :error-message="comuPasswordError"
+      label-visible
+      :disabled="!isIdPasswordEnabled"
     />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useField } from 'vee-validate';
+import VisioConnectionSeparator from './VisioConnectionSeparator.vue';
 
 const { value: comuUrl, errorMessage: comuUrlError } = useField<string>('url');
 const { value: comuPassword, errorMessage: comuPasswordError } =
   useField<string>('meeting_password');
 const { value: comuId, errorMessage: comuIdError } = useField<string>('meeting_platform_id');
+
+const isIdPasswordEnabled = computed(() => {
+  return comuUrl.value === null || comuUrl.value === '';
+});
+const isUrlEnabled = computed(() => {
+  return (
+    (comuPassword.value === null || comuPassword.value === '') &&
+    (comuId.value === null || comuId.value === '')
+  );
+});
 </script>
 
 <style scoped>
 :deep(.fr-input-group) {
   flex-grow: 1;
   min-width: 50%;
+}
+
+:deep(.fr-label) {
+  color: unset;
+}
+
+:deep(.fr-hint-text) {
+  color: var(--text-mention-grey);
 }
 </style>
