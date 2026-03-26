4da634b75f8e8322a6995f9e525f680ac9a7f21c
theodore2leusse <deleussetheodore@gmail.com>
2026-03-26 17:26:52 +0100

    :recycle: (245) use compute_transcription_chunks in speech-to-text pipeline

--- mcr-core/mcr_meeting/app/services/speech_to_text/utils/__init__.py
+++ mcr-core/mcr_meeting/app/services/speech_to_text/utils/__init__.py
@@ -1,6 +1,7 @@
 """Public exports for the speech_to_text utils package."""
 
 from .audio import split_audio_on_timestamps
+from .chunking import compute_transcription_chunks
 from .models import (
     get_diarization_pipeline,
     get_transcription_model,
@@ -8,14 +9,13 @@ from .models import (
 from .vad import (
     convert_to_french_speaker,
     diarize_vad_transcription_segments,
-    get_vad_segments_from_diarization,
 )
 
 __all__ = [
     "split_audio_on_timestamps",
+    "compute_transcription_chunks",
     "get_transcription_model",
     "get_diarization_pipeline",
     "convert_to_french_speaker",
     "diarize_vad_transcription_segments",
-    "get_vad_segments_from_diarization",
 ]
