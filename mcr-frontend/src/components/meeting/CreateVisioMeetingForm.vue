48a384025956613855cba5baa7f9e0562618c772
Alexandre Morin <alexandre.morin@ext.bpifrance.fr>
2026-03-30 09:21:51 +0200

    :bug: add comu url/passcode verication

--- mcr-frontend/src/components/meeting/CreateVisioMeetingForm.vue
+++ mcr-frontend/src/components/meeting/CreateVisioMeetingForm.vue
@@ -75,13 +75,25 @@ import ComuMeetingForm from './visio-modal/ComuMeetingForm.vue';
 import WebconfMeetingForm from './visio-modal/WebconfMeetingForm.vue';
 import WebinaireMeetingForm from './visio-modal/WebinaireMeetingForm.vue';
 import VisioMeetingForm from './visio-modal/VisioMeetingForm.vue';
-import { useForm, useIsFormValid } from 'vee-validate';
+import { useForm, useFormErrors, useFormValues, useIsFormValid } from 'vee-validate';
 import { toTypedSchema } from '@vee-validate/yup';
-import { visioMeetingFieldsToVisioMeetingDto, VisioMeetingSchema } from './meeting.schema';
+import {
+  comuPrivateUrlValidator,
+  visioMeetingFieldsToVisioMeetingDto,
+  VisioMeetingSchema,
+} from './meeting.schema';
+import { useMeetings } from '@/services/meetings/use-meeting';
+import { parseComuUrl } from '@/services/lookup/lookup.service';
+import { useI18n } from 'vue-i18n';
+
 import WebexMeetingForm from './visio-modal/WebexMeetingForm.vue';
 import { useFeatureFlag } from '@/composables/use-feature-flag';
 
 const isWebexEnabled = useFeatureFlag('webex');
+import useToaster from '@/composables/use-toaster';
+
+const { t } = useI18n();
+const toaster = useToaster();
 
 const platformLabels: Record<OnlineMeetingPlatforms, string> = {
   COMU: 'COMU',
@@ -96,6 +108,53 @@ const { defineField, handleSubmit } = useForm({
 });
 
 const [meetingTitle, meetingTitleAttrs] = defineField('name');
+const formValues = useFormValues();
+
+const { lookupMeetingUrlMutation, lookupMeetingByPasscodeMutation } = useMeetings();
+
+const formErrors = useFormErrors();
+
+const onLookupError = () => {
+  toaster.addMessage({
+    description: t('meeting.form.errors.lookup-warning'),
+    type: 'warning',
+  });
+};
+
+const { mutate: lookupByUrl } = lookupMeetingUrlMutation({
+  onError: onLookupError,
+});
+
+const { mutate: lookupByPasscode } = lookupMeetingByPasscodeMutation({
+  onError: onLookupError,
+});
+
+watch(
+  () => formValues.value.url,
+  (newUrl) => {
+    if (newUrl && comuPrivateUrlValidator.test(newUrl)) {
+      lookupByUrl(parseComuUrl(newUrl));
+    }
+  },
+);
+
+function hasNoFieldErrors(...fields: string[]) {
+  return fields.every((field) => !formErrors.value[field]);
+}
+
+watch(
+  () => [formValues.value.meeting_platform_id, formValues.value.meeting_password],
+  ([newId, newPassword]) => {
+    if (
+      selectedPlatform.value === 'COMU' &&
+      newId &&
+      newPassword &&
+      hasNoFieldErrors('meeting_platform_id', 'meeting_password')
+    ) {
+      lookupByPasscode({ comu_meeting_id: newId, passcode: newPassword });
+    }
+  },
+);
 
 const meetingPlatformOptions = OnlineMeetingPlatforms.filter(
   (platform) => platform !== 'WEBEX' || isWebexEnabled.value,
