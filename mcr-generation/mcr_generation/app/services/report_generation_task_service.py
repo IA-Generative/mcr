09e2ec6282cd33c9eb45c167ced0448997b93529
Thomas Metzger <thomas.metzger@theodo.com>
2026-04-21 19:22:23 +0200

    :sparkles: (mcr-generation) enrich Sentry with meeting context on report prerun

--- mcr-generation/mcr_generation/app/services/report_generation_task_service.py
+++ mcr-generation/mcr_generation/app/services/report_generation_task_service.py
@@ -1,21 +1,27 @@
 from typing import Any
 
 import httpx
-from celery.signals import task_failure, task_success
+from celery.signals import task_failure, task_prerun, task_success
 from langfuse import observe
 from loguru import logger
 
+from mcr_generation.app.client.meeting_client import MeetingApiClient
 from mcr_generation.app.configs.settings import ApiSettings
 from mcr_generation.app.exceptions.exceptions import ReportCallbackError
 from mcr_generation.app.schemas.base import BaseReport
 from mcr_generation.app.schemas.celery_types import (
     MCRReportGenerationTasks,
     ReportTypes,
+    extract_report_task_args,
 )
 from mcr_generation.app.services.report_generator import get_generator
 from mcr_generation.app.services.utils.input_chunker import chunk_docx_to_document_list
 from mcr_generation.app.services.utils.s3_service import get_file_from_s3
 from mcr_generation.app.utils.celery_worker import celery_app
+from mcr_generation.app.utils.sentry_context import (
+    gather_meeting_context,
+    set_sentry_meeting_context,
+)
 
 api_settings = ApiSettings()
 
@@ -26,6 +32,7 @@ def generate_report_from_docx(
     meeting_id: int,
     transcription_object_filename: str,
     report_type: str = ReportTypes.DECISION_RECORD.value,
+    owner_keycloak_uuid: str | None = None,
 ) -> BaseReport:
     docx_bytes = get_file_from_s3(transcription_object_filename)
     chunks = chunk_docx_to_document_list(docx_bytes)
@@ -33,6 +40,17 @@ def generate_report_from_docx(
     report_type_enum = ReportTypes(report_type)
     generator = get_generator(report_type_enum)
     return generator.generate(chunks)
+
+
+@task_prerun.connect(sender=generate_report_from_docx)
+def set_sentry_context_before_report_generation(**kwargs: Any) -> None:
+    task_args = extract_report_task_args(kwargs)
+
+    client = MeetingApiClient(task_args.owner_keycloak_uuid)
+    meeting_context = gather_meeting_context(
+        task_args.meeting_id, task_args.owner_keycloak_uuid, client
+    )
+    set_sentry_meeting_context(meeting_context)
 
 
 @task_success.connect
