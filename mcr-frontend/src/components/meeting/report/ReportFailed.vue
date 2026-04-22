875eb02fa33cac916201e13c1f1b370a58d1693a
Léo Germain <leo.germain@theodo.com>
2026-04-22 16:30:10 +0200

    :lipstick: (556): create ReportFailed component

---
+++ mcr-frontend/src/components/meeting/report/ReportFailed.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col items-center gap-8 mx-4">
+    <div class="flex flex-col items-center gap-4">
+      <img
+        src="@dsfr-artwork/pictograms/system/warning.svg?url"
+        role="presentation"
+        class="w-22 h-22"
+      />
+      <div class="text-center">
+        <h3 class="text-xl font-bold mb-6 text-[var(--blue-france-sun-113-625)]">
+          {{ $t('meeting.transcription.transcription-failed.title') }}
+        </h3>
+        <p class="text-lg mb-4 text-[var(--default-text-grey)]">
+          {{ $t('meeting.transcription.transcription-failed.description') }}
+        </p>
+      </div>
+      <DsfrButton @click="$emit('onReset')">
+        {{ $t('meeting.transcription.transcription-failed.button') }}
+      </DsfrButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { DsfrButton } from '@gouvminint/vue-dsfr';
+
+defineProps<{
+  meetingId: number;
+}>();
+defineEmits<{ onReset: [] }>();
+</script>
