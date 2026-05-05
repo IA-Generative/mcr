1a6817735fa65782d93becbdcd9084e258f52844
Léo Germain <leo.germain@theodo.com>
2026-05-05 11:52:53 +0200

    :recycle: (551): conditionally display meeting duration in front matter

--- mcr-frontend/src/components/meeting/MeetingFrontMatterV2.vue
+++ mcr-frontend/src/components/meeting/MeetingFrontMatterV2.vue
@@ -6,28 +6,38 @@
         { text: meeting.name, to: `/meetings/${meeting.id}` },
       ]"
     />
-    <h1 class="fr-text text-4xl font-bold text-grey-200 truncate-title">{{ meeting.name }}</h1>
-    <i18n-t
-      keypath="meeting-v2.details"
-      tag="div"
-      class="text-grey-200"
+    <h1
+      class="fr-text text-4xl font-bold text-grey-200 max-w-[50vw] overflow-hidden whitespace-nowrap text-ellipsis"
     >
-      <template #subtitle>
-        <span>{{ getSubtitleFromPlatformName(meeting.name_platform) }}</span>
-      </template>
-      <template #date>
-        <span class="font-semibold">{{ getCalendarDateFromIso8601(meeting.creation_date) }}</span>
-      </template>
-      <template #time>
-        <span class="font-semibold">{{ getTimeFromIso8601(meeting.creation_date) }}</span>
-      </template>
-      <template #separator>
+      {{ meeting.name }}
+    </h1>
+    <div class="text-grey-200">
+      <i18n-t
+        keypath="meeting-v2.details"
+        tag="span"
+      >
+        <template #subtitle>
+          <span>{{ getSubtitleFromPlatformName(meeting.name_platform) }}</span>
+        </template>
+        <template #date>
+          <span class="font-semibold">{{ getCalendarDateFromIso8601(meeting.creation_date) }}</span>
+        </template>
+        <template #time>
+          <span class="font-semibold">{{ getTimeFromIso8601(meeting.creation_date) }}</span>
+        </template>
+      </i18n-t>
+      <template v-if="meetingDuration">
         <span class="ml-4 mr-4">{{ t('common.pipe') }}</span>
+        <i18n-t
+          keypath="meeting-v2.duration-label"
+          tag="span"
+        >
+          <template #duration>
+            <span class="font-semibold">{{ meetingDuration }}</span>
+          </template>
+        </i18n-t>
       </template>
-      <template #duration>
-        <span class="font-semibold">{{ getMeetingDuration(meeting) }}</span>
-      </template>
-    </i18n-t>
+    </div>
   </div>
 </template>
 
@@ -35,15 +45,19 @@
 import { t } from '@/plugins/i18n';
 import {
   getCalendarDateFromIso8601,
-  getMeetingDuration,
+  calculateDuration,
   getTimeFromIso8601,
 } from '@/services/meetings/meetings-datetime';
 import type { AllMeetingPlatforms, MeetingDetailDto } from '@/services/meetings/meetings.types';
 import { DsfrBreadcrumb } from '@gouvminint/vue-dsfr';
 
-defineProps<{
+const props = defineProps<{
   meeting: MeetingDetailDto;
 }>();
+
+const meetingDuration = computed(
+  () => calculateDuration(props.meeting.start_date, props.meeting.end_date) || undefined,
+);
 
 function getSubtitleFromPlatformName(namePlatform: AllMeetingPlatforms): string {
   switch (namePlatform) {
@@ -58,13 +72,6 @@ function getSubtitleFromPlatformName(namePlatform: AllMeetingPlatforms): string 
 </script>
 
 <style scoped>
-.truncate-title {
-  max-width: 50vw;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-}
-
 :deep(.fr-breadcrumb) {
   margin: 0;
 }
