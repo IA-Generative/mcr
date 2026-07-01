c0b307687aeb6ee9a8b90e542de05e7aa42bd5f7
Thibault Foucart <thibault.foucart@theodo.com>
2026-07-01 16:53:41 +0200

    :recycle: (901): break load_speech_to_text_model

--- mcr-core/mcr_meeting/app/infrastructure/speech_to_text_models.py
+++ mcr-core/mcr_meeting/app/infrastructure/speech_to_text_models.py
@@ -11,9 +11,15 @@ from faster_whisper import WhisperModel
 from loguru import logger
 from pyannote.audio import Pipeline
 
-from mcr_meeting.app.configs.base import Settings, Speech2TextSettings
-from mcr_meeting.app.utils.compute_devices import ComputeDevice
-from mcr_meeting.app.utils.load_speech_to_text_model import load_diarization_pipeline
+from mcr_meeting.app.configs.base import (
+    PyannoteDiarizationParameters,
+    Settings,
+    Speech2TextSettings,
+)
+from mcr_meeting.app.utils.compute_devices import (
+    ComputeDevice,
+    get_default_gpu_device,
+)
 
 settings = Settings()
 speech2text_settings = Speech2TextSettings()
@@ -23,6 +29,42 @@ class WorkerContext(TypedDict):
     device: ComputeDevice
     model: WhisperModel | None
     diarization_pipeline: Pipeline | None
+
+
+def load_whisper_model(device: ComputeDevice) -> WhisperModel:
+    logger.info("Loading Whisper model...")
+    model = WhisperModel(speech2text_settings.STT_MODEL, device=device)
+
+    if not model:
+        raise RuntimeError("Failed to load Whisper model")
+
+    logger.info("Model loaded successfully")
+    return model
+
+
+def load_diarization_pipeline(device: ComputeDevice) -> Pipeline:
+    logger.info("Loading diarization pipeline...")
+
+    diarization_pipeline = Pipeline.from_pretrained(
+        speech2text_settings.DIARIZATION_MODEL,
+        use_auth_token=speech2text_settings.HUGGINGFACE_API_KEY,
+    )
+
+    if device == ComputeDevice.GPU:
+        logger.info("Using GPU for diarization")
+        diarization_pipeline.to(get_default_gpu_device())
+
+    diarization_settings = PyannoteDiarizationParameters()
+    diarization_pipeline.segmentation.min_duration_off = (
+        diarization_settings.min_duration_off
+    )
+    diarization_pipeline.clustering.min_cluster_size = (
+        diarization_settings.min_cluster_size
+    )
+    diarization_pipeline.clustering.threshold = diarization_settings.threshold
+
+    logger.info("Diarization pipeline loaded successfully")
+    return diarization_pipeline
 
 
 context: WorkerContext = {
